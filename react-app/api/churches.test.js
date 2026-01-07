5332c3fc460e4cad2aed144c0b488fb950ece82d
SagiBeno <benosagi321@gmail.com>
2026-01-07 13:14:43 +0100

    Test GET

--- react-app/api/churches.test.js
+++ react-app/api/churches.test.js
@@ -3,6 +3,8 @@ import { describe, it, expect, test } from 'vitest';
 import { Pool } from '@neondatabase/serverless';
 import handler from './churches';
 
+var pool = new Pool({ connectionString: process.env.DATABASE_CHURCHES_URL });
+
 describe('Fake Test Suite', () => {
     test('2+3=5', () => {
         expect(2+3).toBe(5);
@@ -10,5 +12,14 @@ describe('Fake Test Suite', () => {
 });
 
 describe('Test Suite for REST API endpoints', () => {
-    
+    test('GET /api/churches', async () => {
+        fetch('https://church-vercel-neon-mu.vercel.app/api/churches')
+        //  .then(console.log)
+        //  .catch(console.warn)
+            .finally(() => {})
+    });
+
+    test('POST /api/churches', () => {
+        // TODO - use pool and handler(req, res) to mock POST
+    });
 });
