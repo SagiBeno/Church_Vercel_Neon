cab252822e40610d3182eb8278f59c3e647792ca
SagiBeno <benosagi321@gmail.com>
2026-01-07 12:59:32 +0100

    Prepare test

--- react-app/api/churches.test.js
+++ react-app/api/churches.test.js
@@ -1,7 +1,14 @@
 import { describe, it, expect, test } from 'vitest';
 
+import { Pool } from '@neondatabase/serverless';
+import handler from './churches';
+
 describe('Fake Test Suite', () => {
     test('2+3=5', () => {
         expect(2+3).toBe(5);
-    })
-})
+    });
+});
+
+describe('Test Suite for REST API endpoints', () => {
+    
+});
