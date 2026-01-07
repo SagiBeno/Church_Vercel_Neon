478521e9008289fc52d30599c108eddc37a6b934
SagiBeno <benosagi321@gmail.com>
2026-01-07 13:32:17 +0100

    Mock test POST with valid data

--- react-app/api/churches.test.js
+++ react-app/api/churches.test.js
@@ -1,9 +1,18 @@
-import { describe, it, expect, test } from 'vitest';
+import { describe, it, expect, test, vi } from 'vitest';
 
 import { Pool } from '@neondatabase/serverless';
 import handler from './churches';
 
 var pool = new Pool({ connectionString: process.env.DATABASE_CHURCHES_URL });
+
+// Mock Neon
+vi.mock('@neondatabase/serverless', () => ({
+    Pool: vi.fn()
+}));
+
+const mockPool = {
+    query: vi.fn()
+}
 
 describe('Fake Test Suite', () => {
     test('2+3=5', () => {
@@ -12,14 +21,28 @@ describe('Fake Test Suite', () => {
 });
 
 describe('Test Suite for REST API endpoints', () => {
-    test('GET /api/churches', async () => {
+    test('GET /api/churches', () => {
         fetch('https://church-vercel-neon-mu.vercel.app/api/churches')
         //  .then(console.log)
         //  .catch(console.warn)
             .finally(() => {})
     });
 
-    test('POST /api/churches', () => {
+    test('POST /api/churches', async () => {
         // TODO - use pool and handler(req, res) to mock POST
+        const mockReq = {
+            method: 'POST',
+            body: { name: 'Test Church', is_christian: false }
+        }
+
+        const mockRes = {
+            status: vi.fn(),
+            json: vi.fn()
+        }
+        mockPool.query.mockResolvedValue( { data: { rows: [ {id: 1} ] } } );
+
+        const result = await handler(mockReq, mockRes);
+        const resBodyJson = result?.json;
+        expect(resBodyJson).not.toBeNull();
     });
 });
