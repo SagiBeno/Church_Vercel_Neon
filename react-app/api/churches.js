1d3b1a598ff24594a3acf59f19c3cf4f70bec00f
SagiBeno <benosagi321@gmail.com>
2026-01-07 08:56:55 +0100

    Scale endpoint

--- react-app/api/churches.js
+++ react-app/api/churches.js
@@ -8,14 +8,35 @@
  */
 
 import { Pool } from '@neondatabase/serverless';
-var pool = new Pool( { connectionString: process.env.DATABASE_CHURCHES_URL } );
+var pool = new Pool({ connectionString: process.env.DATABASE_CHURCHES_URL });
 
-export default async function handler (req, res) {
-    //console.log('pool: ', pool)
+export default async function handler(req, res) {
 
-    const sql = `SELECT id, name, is_christian, created_at FROM public.churches`;
+    let sql = '';
+    let data = [];
+    let error = null; // TODO - handle errors!
 
-    const queryResult = await pool.query(sql);
-    console.log('queryResult: ', queryResult)
-    return res.status(200).json( { queryResult } );
+
+    if (req.method == 'GET') {
+        sql = `SELECT id, name, is_christian, created_at FROM public.churches`;
+        data = await pool.query(sql);
+        console.log('data: ', data)
+        return res.status(200).json({ sql, data, error });
+    }
+
+    if (req.method == 'POST') {
+        // TODO
+        sql = `SELECT id, name, is_christian, created_at FROM public.churches`;
+        data = await pool.query(sql);
+        console.log('data: ', data)
+        return res.status(200).json({ sql, data, error });
+    }
+
+    if (req.method == 'PATCH') {
+        return res.status(200).json({ sql, data, error }); // TODO
+    }
+
+    if (req.method == 'DELETE') {
+        return res.status(200).json({ sql, data, error }); // TODO
+    }
 }
